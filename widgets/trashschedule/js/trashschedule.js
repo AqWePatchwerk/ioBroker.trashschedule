e9151a39c01ef94578e75f2ff369a67d93bba61c
Fieldy <dennismaass1989@gmail.com>
2023-07-16 23:01:50 +0200

    dfsdf

--- widgets/trashschedule/js/trashschedule.js
+++ widgets/trashschedule/js/trashschedule.js
@@ -170,7 +170,7 @@ $.extend(
 );
 
 vis.binds['trashschedule'] = {
-    version: '2.2.0',
+    version: '2.2.1',
     showVersion: function () {
         if (vis.binds['trashschedule'].version) {
             console.log('Version trashschedule: ' + vis.binds['trashschedule'].version);
